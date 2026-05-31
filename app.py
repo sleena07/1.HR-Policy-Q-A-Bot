472c624d839111c55df424cc1e54270ea60aa708
sleena07 <147167325+sleena07@users.noreply.github.com>
2026-05-31 11:16:45 +0530

    change

--- app.py
+++ app.py
@@ -11,11 +11,10 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.retrievers import MultiQueryRetriever
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferWindowMemory
 from langchain_core.prompts import PromptTemplate
-
 
 # ============================================
 # ENVIRONMENT VARIABLES
